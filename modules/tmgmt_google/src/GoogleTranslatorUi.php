f86c85a35d7a9fdef02da14dcf88c9f585073790
Andrew Larcombe <andrew@andrewl.net>
2015-09-25 12:50:13 +0200

    added google translate

---
+++ modules/tmgmt_google/src/GoogleTranslatorUi.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\tmgmt_google\GoogleTranslatorUi.
+ */
+
+namespace Drupal\tmgmt_google;
+
+use Drupal\tmgmt\TranslatorInterface;
+use Drupal\tmgmt\TranslatorPluginUiBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Google translator UI.
+ */
+class GoogleTranslatorUi extends TranslatorPluginUiBase {
+
+  /**
+   * Overrides TMGMTDefaultTranslatorUIController::pluginSettingsForm().
+   */
+  public function pluginSettingsForm(array $form, FormStateInterface $form_state, TranslatorInterface $translator, $busy = FALSE) {
+    $form['api_key'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Google API key'),
+      '#default_value' => $translator->getSetting('api_key'),
+      '#description' => t('Please enter your Google API key or visit <a href="@url">Google APIs console</a> to create new one.',
+        array('@url' => 'https://code.google.com/apis/console')),
+    );
+    return parent::pluginSettingsForm($form, $form_state, $translator);
+  }
+
+}
